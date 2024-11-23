1a902db11559e2846187fb1675ee41b71ce412d0
Tara Druffel <taradruffel@outlook.com>
2024-11-23 16:13:39 +0100

    arst

--- modules/tools/gptel.el
+++ modules/tools/gptel.el
@@ -2,5 +2,4 @@
 :ensure t)
 
 (setq gptel-default-mode 'text-mode)
-(setq gptel-api-key "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")
 
