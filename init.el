31f1d30725d38101c1ccd9fa9d8dc31f53adf19b
Tara Druffel <taradruffel@outlook.com>
2024-11-28 15:29:16 +0100

    test

--- init.el
+++ init.el
@@ -37,14 +37,94 @@
 (keymap-set symbol-overlay-map "C-o" #'casual-symbol-overlay-tmenu)
 (keymap-global-set "C-o" #'casual-editkit-main-tmenu)
 
+(use-package modus-themes
+      :custom
+      (modus-themes-italic-constructs t)
+      (modus-themes-bold-constructs t)
+      (modus-themes-mixed-fonts t)
+      (modus-themes-headings '((1 . (1.5))
+                               (2 . (1.3))
+                               (t . (1.1))))
+      (modus-themes-to-toggle
+       '(modus-operandi-tinted modus-vivendi-tinted))
+      :bind
+      (("C-c w m" . modus-themes-toggle)
+       ("C-c w M" . modus-themes-select)))
+
+  (use-package ef-themes :ensure t)
+  (setq custom-theme 'modus-operandi-tinted)
+
+  (defun apply-modus-operandi-tinted-palette (frame)
+    (with-selected-frame frame
+      (load-theme custom-theme t)))
+  (if (daemonp)
+      (add-hook 'after-make-frame-functions #'apply-modus-operandi-tinted-palette)
+    (load-theme custom-theme t))
+
+  (defun set-default-font (frame)
+    "Set the default font for the FRAME."
+    (with-selected-frame frame
+      (set-face-attribute 'default nil
+                          :family "Iosevka Aile" ; Replace with your font name
+                          :height 160
+                          )))         ; Adjust the height as needed
+
+  (if (daemonp)
+      (add-hook 'after-make-frame-functions #'set-default-font)
+    (set-default-font (selected-frame)))
+
+
+
+    ;; Distraction-free writing
+    (defun ews-distraction-free ()
+      "Distraction-free writing environment using Olivetti package."
+      (interactive)
+      (if (equal olivetti-mode nil)
+          (progn
+            (window-configuration-to-register 1)
+            (delete-other-windows)
+            (text-scale-set 1)
+            (olivetti-mode t))
+        (progn
+          (if (eq (length (window-list)) 1)
+              (jump-to-register 1))
+          (olivetti-mode 0)
+          (text-scale-set 0))))
+
+    (use-package olivetti
+      :ensure t
+      :demand t
+      :bind
+      (("<f9>" . ews-distraction-free)))
+(setq olivetti-body-width 90)
+(setq olivetti-minium-Body-width 60)
+
+(use-package spacious-padding :ensure t)
+(require 'spacious-padding)
+
+;; These are the default values, but I keep them here for visibility.
+(setq spacious-padding-widths
+      '( :internal-border-width 15
+         :header-line-width 4
+         :mode-line-width 6
+         :tab-width 4
+         :right-divider-width 30
+         :scroll-bar-width 8
+         :fringe-width 8))
+
+(spacious-padding-mode 1)
+
+;; Set a key binding if you need to toggle spacious padding.
+(define-key global-map (kbd "<f8>") #'spacious-padding-mode)
+
+(setopt sentence-end-double-space nil)
+
 (setq xref-search-program 'ripgrep)
 (setq grep-command "rg -nS --noheading")
 
 (savehist-mode)
 
 (windmove-default-keybindings 'control)
-
-(setopt sentence-end-double-space nil)
 
 (when (display-graphic-p)
   (context-menu-mode))
@@ -101,111 +181,87 @@
 (setopt display-time-interval 1)
 (display-time-mode)
 
-(use-package modus-themes
-      :custom
-      (modus-themes-italic-constructs t)
-      (modus-themes-bold-constructs t)
-      (modus-themes-mixed-fonts t)
-      (modus-themes-headings '((1 . (1.5))
-                               (2 . (1.3))
-                               (t . (1.1))))
-      (modus-themes-to-toggle
-       '(modus-operandi-tinted modus-vivendi-tinted))
-      :bind
-      (("C-c w m" . modus-themes-toggle)
-       ("C-c w M" . modus-themes-select)))
+(use-package expand-region
+  :ensure t)
+(require 'expand-region)
 
-  (use-package ef-themes :ensure t)
-  (setq custom-theme 'modus-operandi-tinted)
+(use-package exec-path-from-shell :ensure t)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
 
-  (defun apply-modus-operandi-tinted-palette (frame)
-    (with-selected-frame frame
-      (load-theme custom-theme t)))
-  (if (daemonp)
-      (add-hook 'after-make-frame-functions #'apply-modus-operandi-tinted-palette)
-    (load-theme custom-theme t))
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
 
-  (defun set-default-font (frame)
-    "Set the default font for the FRAME."
-    (with-selected-frame frame
-      (set-face-attribute 'default nil
-                          :family "Monaspace Neon Var" ; Replace with your font name
-                          :height 160
-                          )))         ; Adjust the height as needed
+(setq which-key-sort-order 'which-key-key-order-alpha)
+(setq which-key-max-display-columns 2)
+(setq which-key-popup-type 'side-window)
+(setq which-key-side-window-location 'right)
+(setq which-key-side-window-max-height 0.25)
+(setq which-key-side-window-max-width 0.33)
 
-  (if (daemonp)
-      (add-hook 'after-make-frame-functions #'set-default-font)
-    (set-default-font (selected-frame)))
+(which-key-add-key-based-replacements "C-x 3" "Open Split")
+(which-key-add-key-based-replacements "C-x C-f" "[F]ile")
 
+(define-prefix-command 'file-prefix-map)
+(global-set-key (kbd "C-x C-f") 'file-prefix-map)
+(define-key file-prefix-map (kbd "f") 'find-file)           ; Open file
+(define-key file-prefix-map (kbd "r") 'recentf)            ; Open recent files
 
+(global-set-key (kbd "C-=") (cons "Expand Selection" 'er/expand-region))
+(global-set-key (kbd "s-v") 'cua-paste)
+(global-set-key (kbd "C-v") 'scroll-up-command)
+(global-set-key (kbd "M-s g") 'grep-find)
+(global-set-key (kbd "C-x ,") 'ibuffer)
 
-    ;; Distraction-free writing
-    (defun ews-distraction-free ()
-      "Distraction-free writing environment using Olivetti package."
-      (interactive)
-      (if (equal olivetti-mode nil)
-          (progn
-            (window-configuration-to-register 1)
-            (delete-other-windows)
-            (text-scale-set 1)
-            (olivetti-mode t))
-        (progn
-          (if (eq (length (window-list)) 1)
-              (jump-to-register 1))
-          (olivetti-mode 0)
-          (text-scale-set 0))))
+(which-key-add-key-based-replacements "C-c o" "[O]pen")
+(which-key-add-key-based-replacements "C-c c" "[C]ode")
+(which-key-add-key-based-replacements "C-c j" "[J]ump to")
+(which-key-add-key-based-replacements "C-c l" "[L]edger")
+(which-key-add-key-based-replacements "C-c n" "[N]otes")
+(which-key-add-key-based-replacements "C-c x" "[X] Capture")
 
-    (use-package olivetti
-      :ensure t
-      :demand t
-      :bind
-      (("<f9>" . ews-distraction-free)))
+(define-prefix-command 'gptel-prefix-map)
+(global-set-key (kbd "C-c oc") 'gptel-prefix-map)
+(which-key-add-key-based-replacements "C-c oc" "AI [C]hat")
+(define-key gptel-prefix-map (kbd "m") (cons "Open AI [M]enu" 'gptel-menu))
+(define-key gptel-prefix-map (kbd "s") (cons "[S]end to AI" 'gptel-send))
+(define-key gptel-prefix-map (kbd "C") (cons "AI [C]hat" 'gptel))
 
-(use-package spacious-padding :ensure t)
-(require 'spacious-padding)
+(global-set-key (kbd "C-c cf") 'eglot-format)
+(global-set-key (kbd "C-c cc") 'compile)
+(global-set-key (kbd "C-c ca") 'eglot-code-actions)
+(global-set-key (kbd "M-[") 'flymake-goto-prev-error)
+(global-set-key (kbd "M-]") 'flymake-goto-next-error)
 
-;; These are the default values, but I keep them here for visibility.
-(setq spacious-padding-widths
-      '( :internal-border-width 15
-         :header-line-width 4
-         :mode-line-width 6
-         :tab-width 4
-         :right-divider-width 30
-         :scroll-bar-width 8
-         :fringe-width 8))
+(global-set-key (kbd "C-c lr") 'hledger-run-command)
+(global-set-key (kbd "C-c le") 'hledger-jentry)
 
-(spacious-padding-mode 1)
+(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
 
-;; Set a key binding if you need to toggle spacious padding.
-(define-key global-map (kbd "<f8>") #'spacious-padding-mode)
-
-(load-file (expand-file-name "modules/lang/markdown.el" user-emacs-directory))
-(load-file (expand-file-name "modules/lang/org.el" user-emacs-directory))
-(load-file (expand-file-name "modules/lang/ledger.el" user-emacs-directory))
-(load-file (expand-file-name "modules/lang/clojure.el" user-emacs-directory))
+(global-set-key (kbd "C-c ns") 'org-search-view)
+(global-set-key (kbd "C-c ncd") 'org-timeblock)
+(global-set-key (kbd "C-c ncm") 'cfw:open-org-calendar)
+(global-set-key (kbd "C-c na") 'org-agenda)
+(global-set-key (kbd "C-c x") 'org-capture)
 
 (load-file (expand-file-name "modules/tools/eglot.el" user-emacs-directory))
-(load-file (expand-file-name "modules/tools/devdocs.el" user-emacs-directory))
-(load-file (expand-file-name "modules/tools/term.el" user-emacs-directory))
-(load-file (expand-file-name "modules/tools/work/work.el" user-emacs-directory))
-(load-file (expand-file-name "modules/tools/gptel.el" user-emacs-directory))
-(load-file (expand-file-name "modules/tools/rss.el" user-emacs-directory))
 
-(setq hledger-currency-string "EUR"
-      hledger-extra-args ""
-      hledger-extrapolate-savings-period 12
-      hledger-extrapolate-savings-rate 7.0
-      hledger-ratios-assets-accounts "Assets"
-      hledger-ratios-debt-accounts "Liabilities"
-      hledger-ratios-essential-expense-accounts ""
-      hledger-ratios-income-accounts
-      "Revenue:Gehalt Revenue:Sonstiges Revenue:Netflix Revenue:Refunds"
-      hledger-ratios-liquid-asset-accounts "Assets:Cash Assets:Checking Assets:Savings"
-      hledger-show-expanded-report t
-      hledger-top-asset-account "Assets"
-      hledger-top-expense-account "Expenses"
-      hledger-top-income-account "Revenue"
-      hledger-year-of-birth 1997)
+(load-file (expand-file-name "modules/lang/markdown.el" user-emacs-directory))
+
+(load-file (expand-file-name "modules/lang/org.el" user-emacs-directory))
+
+(load-file (expand-file-name "modules/lang/ledger.el" user-emacs-directory))
+
+(load-file (expand-file-name "modules/lang/clojure.el" user-emacs-directory))
+
+(use-package sly :ensure t)
+
+(use-package magit :ensure t
+  :bind ((
+          "C-x g" . magit-status
+          ))
+  )
 
 (use-package emacs
   :config
@@ -240,68 +296,13 @@
         (java "https://github.com/tree-sitter/tree-sitter-java" "master" "src")
         (clojure "https://github.com/sogaiu/tree-sitter-clojure")))
 
-(use-package expand-region
-  :ensure t)
-(require 'expand-region)
+(load-file (expand-file-name "modules/tools/term.el" user-emacs-directory))
 
-(use-package exec-path-from-shell :ensure t)
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
+(load-file (expand-file-name "modules/tools/work/work.el" user-emacs-directory))
 
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
+(load-file (expand-file-name "modules/tools/gptel.el" user-emacs-directory))
 
-(setq which-key-sort-order 'which-key-key-order-alpha)
-(setq which-key-max-display-columns 2)
-(setq which-key-popup-type 'side-window)
-(setq which-key-side-window-location 'right)
-(setq which-key-side-window-max-height 0.25)
-(setq which-key-side-window-max-width 0.33)
-
-(which-key-add-key-based-replacements "C-x 3" "Open Split")
-(which-key-add-key-based-replacements "C-x C-f" "[F]ile")
-
-(define-prefix-command 'file-prefix-map)
-(global-set-key (kbd "C-x C-f") 'file-prefix-map)
-(define-key file-prefix-map (kbd "f") 'find-file)           ; Open file
-(define-key file-prefix-map (kbd "r") 'recentf)            ; Open recent files
-
-(global-set-key (kbd "C-=") (cons "Expand Selection" 'er/expand-region))
-(global-set-key (kbd "s-v") 'cua-paste)
-(global-set-key (kbd "C-v") 'scroll-up-command)
-(global-set-key (kbd "M-s g") 'grep-find)
-
-(which-key-add-key-based-replacements "C-c o" "[O]pen")
-(which-key-add-key-based-replacements "C-c c" "[C]ode")
-(which-key-add-key-based-replacements "C-c j" "[J]ump to")
-(which-key-add-key-based-replacements "C-c l" "[L]edger")
-(which-key-add-key-based-replacements "C-c n" "[N]otes")
-(which-key-add-key-based-replacements "C-c x" "[X] Capture")
-
-(define-prefix-command 'gptel-prefix-map)
-(global-set-key (kbd "C-c oc") 'gptel-prefix-map)
-(which-key-add-key-based-replacements "C-c oc" "AI [C]hat")
-(define-key gptel-prefix-map (kbd "m") (cons "Open AI [M]enu" 'gptel-menu))
-(define-key gptel-prefix-map (kbd "s") (cons "[S]end to AI" 'gptel-send))
-(define-key gptel-prefix-map (kbd "C") (cons "AI [C]hat" 'gptel))
-
-(global-set-key (kbd "C-c cf") 'eglot-format)
-(global-set-key (kbd "C-c cc") 'compile)
-(global-set-key (kbd "C-c ca") 'eglot-code-actions)
-(global-set-key (kbd "M-[") 'flymake-goto-prev-error)
-(global-set-key (kbd "M-]") 'flymake-goto-next-error)
-
-(global-set-key (kbd "C-c lr") 'hledger-run-command)
-(global-set-key (kbd "C-c le") 'hledger-jentry)
-
-(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
-
-(global-set-key (kbd "C-c ns") 'org-search-view)
-(global-set-key (kbd "C-c ncd") 'org-timeblock)
-(global-set-key (kbd "C-c ncm") 'cfw:open-org-calendar)
-(global-set-key (kbd "C-c na") 'org-agenda)
-(global-set-key (kbd "C-c x") 'org-capture)
+(load-file (expand-file-name "modules/tools/rss.el" user-emacs-directory))
 
 (setq org-safe-remote-resources
    '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
